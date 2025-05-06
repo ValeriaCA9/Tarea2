d292ab6308c8dc957e02b774b8e0c88f131e3b94
ValeriaCA9 <vale38646@gmail.com>
2025-05-06 14:56:10 -0600

    Update ProblemasSimples1.c

--- ProblemasSimples1.c
+++ ProblemasSimples1.c
@@ -1,1 +1,27 @@
+//Escriba un diagrama de flujo tal que dado como datos dos numeros y 
+//calcular la suma, resta y multiplicaci´on de dichos numeros.
+//Cruz Alegria Valeria Montserrat 
 
+#include <stdio.h>
+
+int main ()
+{
+    int suma = 0,resta = 0, multiplicacion = 0, cifra1, cifra2;
+    
+    printf("primer cifra: ");
+    scanf("%d", &cifra1);
+    printf("segunda cifra: ");
+    scanf("%d", &cifra2);
+    
+    suma=cifra1+cifra2;
+    resta=cifra1-cifra2;
+    multiplicacion=cifra1*cifra2;
+    
+    printf("suma: %d", suma);
+    printf(" resta: %d", resta);
+    printf(" multiplicación: %d", multiplicacion);
+    
+    return 0;
+   
+    
+}
