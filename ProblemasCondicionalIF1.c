17171037a817dbfff1ddf09782be82e8b1b84be6
ValeriaCA9 <vale38646@gmail.com>
2025-05-06 15:48:42 -0600

    Update ProblemasCondicionalIF1.c

--- ProblemasCondicionalIF1.c
+++ ProblemasCondicionalIF1.c
@@ -1,1 +1,28 @@
+//En un negocio de productos electrodom´esticos aplican un descuento del
+//8% a todos aquellos clientes cuya compra es superior a $2,500. Dado como
+//dato el monto de la compra del cliente, calcule lo que el cliente debe pagar.
+//Hacer el diagrama de flujo correspondiente.
 
+// Cruz Alegria Valeria Montserrat 
+
+#include <stdio.h>
+
+int main ()
+{
+    float cantidadfinal = 0.0, dinero = 0.0;
+    
+    printf("valor de compra: ");
+    scanf("%f", &dinero);
+    
+    if(dinero>2500)
+    {
+        cantidadfinal=dinero-(dinero*.08);
+        printf("cantidad final : %f", cantidadfinal);
+    }
+    else
+    {
+        printf("cantidad final : %f", dinero);
+        return 0;
+    }
+    
+}
