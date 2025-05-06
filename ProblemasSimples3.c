64be8c995cb3158c5c52986bc787540ea5fb3966
ValeriaCA9 <vale38646@gmail.com>
2025-05-06 15:18:15 -0600

    Update ProblemasSimples3.c

--- ProblemasSimples3.c
+++ ProblemasSimples3.c
@@ -1,1 +1,23 @@
+//En una Casa de Cambio necesitan construir un programa que dado como
+//dato una cantidad expresada en d´olares, convierta esa cantidad en pesos.
+//Construir el diagrama de flujo correspondiente. Tomar en cuenta que:
+//1dolar ↢ 11.96pesos.
+//Cruz Alegria Valeria Montserrat 
 
+#include <stdio.h>
+
+int main ()
+{
+    float pesos = 0.0, dolares;
+    
+    printf("cantidad de dolares: ");
+    scanf("%f", &dolares);
+    
+    pesos= 11.96*dolares;
+    
+    printf("equivalente a esta cantidad de pesos : %f", pesos);
+
+    return 0;
+   
+    
+}
