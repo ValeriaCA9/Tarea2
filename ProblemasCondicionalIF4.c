fd93c0e9826e3cf20f3b5607473c4571caae55d9
ValeriaCA9 <vale38646@gmail.com>
2025-05-07 12:52:18 -0600

    Update ProblemasCondicionalIF4.c

--- ProblemasCondicionalIF4.c
+++ ProblemasCondicionalIF4.c
@@ -1,1 +1,29 @@
+//Construir un diagrama de flujo que pueda determinar, dados dos n´umeros
+//enteros, si un n´umero es divisor del otro.
 
+// Cruz Alegría Valeria Montserrat 
+
+#include <stdio.h>
+
+int main ()
+{
+    int a, b, resultado = 0;
+    
+    printf("cifra 1: ");
+    scanf("%d", &a);
+    printf("cifra 2: ");
+    scanf("%d", &b);
+    
+    resultado= a % b;
+    
+    if(resultado!=0)
+    {
+        printf("son divisores");
+    }
+    else
+    {
+        printf("no son divisores");
+    }
+    
+return 0;
+}
