dc2f3ef55da62a95dc175089da34973223253d22
ValeriaCA9 <vale38646@gmail.com>
2025-05-16 08:01:49 -0600

    Update ProblemaArreglos4.c

--- ProblemaArreglos4.c
+++ ProblemaArreglos4.c
@@ -1,1 +1,20 @@
-//Con un while y un if (2 lineas mas que el for)
+
+// Cruz Alegria Valeria Montserrat
+#include <stdio.h>
+
+void main()
+{
+    int A[100];
+    int i, par = 2;
+
+    for(i = 0; i < 100; i++)
+    {
+        A[i] = par;
+        par = par + 2;
+    }
+
+    for(i = 0; i < 100; i++)
+    {
+        printf("%d = %d\n", i, A[i]);
+    }
+}
