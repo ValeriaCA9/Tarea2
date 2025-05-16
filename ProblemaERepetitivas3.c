df451f9ad47261030da6b94d263a6598e2033f5f
ValeriaCA9 <vale38646@gmail.com>
2025-05-16 07:58:23 -0600

    Update ProblemaERepetitivas3.c

--- ProblemaERepetitivas3.c
+++ ProblemaERepetitivas3.c
@@ -1,1 +1,17 @@
+// Cruz Alegria Valeria Montserrat
 
+
+#include <stdio.h>
+
+void main()
+{
+    int suma = 0, i = 10;
+
+    while(i <= 50)
+    {
+        suma += i;
+        i += 2;
+    }
+
+    printf("La suma de pares entre 10 y 50  %d\n", suma);
+}
