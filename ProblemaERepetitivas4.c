756ace0b100b50c571283188018d414b7975fc4f
ValeriaCA9 <vale38646@gmail.com>
2025-05-16 07:58:51 -0600

    Update ProblemaERepetitivas4.c

--- ProblemaERepetitivas4.c
+++ ProblemaERepetitivas4.c
@@ -1,1 +1,19 @@
+// Cruz Alegria Valeria Montserrat
 
+#include <stdio.h>
+
+void main()
+{
+    int N = 0, i = 1, producto = 1;
+
+    printf("Ingrese N: ");
+    scanf("%d", &N);
+
+    while(i <= N)
+    {
+        producto *= i;
+        i++;
+    }
+
+    printf("producto: %d\n", producto);
+}
