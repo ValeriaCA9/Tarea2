4541209db925dc54b0e7d13f89645e8845f06c20
ValeriaCA9 <vale38646@gmail.com>
2025-05-16 07:59:31 -0600

    Update ProblemaArreglos1.c

--- ProblemaArreglos1.c
+++ ProblemaArreglos1.c
@@ -1,1 +1,20 @@
+// Cruz Alegria Valeria Montserrat
 
+#include <stdio.h>
+
+void main()
+{
+    int A[100], n, suma = 0;
+
+    printf("tamaño arreglo: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("A[%d]: ", i);
+        scanf("%d", &A[i]);
+        suma += A[i];
+    }
+
+    printf("suma: %d\n", suma);
+}
