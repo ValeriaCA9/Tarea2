ae5f72d7ec8ef6e9a29c2b9e6b74bd2b8504acc4
ValeriaCA9 <vale38646@gmail.com>
2025-05-16 07:28:14 -0600

    Update ProblemaERepetitivas1.c

--- ProblemaERepetitivas1.c
+++ ProblemaERepetitivas1.c
@@ -1,1 +1,19 @@
+// Cruz Alegria Valeria Montserrat
 
+
+#include <stdio.h>
+
+void main()
+{
+    int N = 0, i = 1, resultado = 0;
+
+    printf("Ingrese un número: ");
+    scanf("%d", &N);
+
+    while(i <= 10)
+    {
+        resultado = N * i;
+        printf("%d x %d = %d\n", N, i, resultado);
+        i++;
+    }
+}
