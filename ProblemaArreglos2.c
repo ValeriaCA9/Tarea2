90348f7cdc3428df9c18269e8897d1655335ba7c
ValeriaCA9 <vale38646@gmail.com>
2025-05-16 08:00:05 -0600

    Update ProblemaArreglos2.c

--- ProblemaArreglos2.c
+++ ProblemaArreglos2.c
@@ -1,1 +1,28 @@
+// Cruz Alegria Valeria Montserrat
 
+#include <stdio.h>
+
+void main()
+{
+    int A[100], n, repetido, c = 0;
+
+    printf("tamano arreglo: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("A[%d]: ", i);
+        scanf("%d", &A[i]);
+    }
+
+    printf("Número repetido: ");
+    scanf("%d", &repetido);
+
+    for(int i = 0; i < n; i++)
+    {
+        if(A[i] == repetido)
+            c++;
+    }
+
+    printf("se repite %d veces\n", c);
+}
