e740c31f5a38f854064d206fbcec1f692cdc4175
ValeriaCA9 <vale38646@gmail.com>
2025-05-16 07:57:51 -0600

    Update ProblemaERepetitivas2.c

--- ProblemaERepetitivas2.c
+++ ProblemaERepetitivas2.c
@@ -1,1 +1,25 @@
+// Cruz Alegria Valeria Montserrat
 
+#include <stdio.h>
+
+void main()
+{
+    int num = 0, i = 0, positivos = 0, negativos = 0, ceros = 0;
+
+    while(i < 100)
+    {
+        printf("Número %d: ", i + 1);
+        scanf("%d", &num);
+
+        if(num > 0)
+            positivos++;
+        else if(num < 0)
+            negativos++;
+        else
+            ceros++;
+
+        i++;
+    }
+
+    printf("positivos: %d\nnegativos: %d\nceros: %d\n", positivos, negativos, ceros);
+}
